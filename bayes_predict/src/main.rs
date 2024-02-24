be36d6fb5ce94b6359bac6cf417953c869629f14
Kevin Mazzarella <kmazza2@uic.edu>
2024-02-24 17:23:58 -0600

    Consolidate trapezoid tests

--- bayes_predict/src/main.rs
+++ bayes_predict/src/main.rs
@@ -32,28 +32,40 @@ fn trapezoid(f: fn(f64) -> f64, a: f64, b: f64, eps: f64, max_steps: u64) -> Res
 }
 
 #[test]
-fn test_trapezoid_const() {
+fn test_trapezoid() {
     match trapezoid(|_| 1.0, 2.0, 21.0, 1.0E-13_f64, 10000) {
         Ok(val) => assert!((19.0 - val).abs() < 0.00001),
         Err(s) => panic!("{}", s)
     };
-}
-
-#[test]
-fn test_trapezoid_cos() {
     match trapezoid(|x| x.cos(), 0.35, 11.1, 1.0E-13_f64, 10000) {
         Ok(val) => assert!((-1.337450395659441 - val).abs() < 0.00001),
         Err(s) => panic!("{}", s)
     };
-}
-
-#[test]
-fn test_trapezoid_exp() {
     match trapezoid(|x| x.exp(), 2.39, 5.28, 1.0E-13_f64, 10000) {
         Ok(val) => assert!((185.4563803458403 - val).abs() < 0.00001),
         Err(s) => panic!("{}", s)
     };
 }
+
+fn log_gamma(x: f64) -> f64 {
+    assert!(x > 0.0);
+    const COEF: [f64; 14] = [
+        57.1562356658629235_f64, -59.5979603554754912_f64, 14.1360979747417471_f64,
+        -0.491913816097620199_f64, 0.339946499848118887E-4_f64, 0.465236289270485756E-4_f64,
+        -0.983744753048795646E-4_f64, 0.158088703224912494E-3_f64, -0.210264441724104883E-3_f64,
+        0.217439618115212643E-3_f64, -0.164318106536763890E-3_f64, 0.844182239838527433E-4_f64,
+        -0.261908384015814087E-4_f64, 0.368991826595316234E-5_f64];
+    const STP: f64 = 2.5066282746310005_f64;
+    let mut y = x;
+    let tmp = x + 5.24218750000000000_f64;
+    let tmp = (x + 0.5) * tmp.ln() - tmp;
+    let mut ser = 0.999999999999997092_f64;
+    for j in 0..14 {
+        y += 1.0;       
+        ser += COEF[j] / y; 
+    }                       
+    tmp + (STP * ser / x).ln()
+}       
 
 fn nchoosek(n: usize, k: usize) -> usize {
     (log_gamma((n + 1) as f64) - log_gamma((k + 1) as f64) - log_gamma((n - k + 1) as f64))
